c7f1a9d54a24a4890d741bba64c71b35451e8c94
羽衣尘 <lilyorlucy@tom.com>
2022-10-08 17:51:21 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,15 +1,43 @@
 
-addEventListener("fetch",  (event) => {
-  let url = new URL(event.request.url);
-  url.hostname = "source.unsplash.com";
-  let request = new Request(url, event.request);
+// addEventListener("fetch",  (event) => {
+//   let url = new URL(event.request.url);
+//   url.hostname = "source.unsplash.com";
+//   let request = new Request(url, event.request);
   
-  const handleRequest = async (req) => {
-    const res = await fetch(request);
-    if (res.status === 302) {
+//   const handleRequest = async (req) => {
+//     const res = await fetch(request);
+//     if (res.status === 302) {
+//         return fetch(res.headers.get('location'));
+//     }
+//     return res
+//   }
+//   event.respondWith(handleRequest(request));
+// });
+
+// export default {
+//   async fetch(request, env) {
+//     let url = new URL(request.url);
+//     if (url.pathname.startsWith('/')) {
+//       url.hostname = 'exepmle.com'
+//       let new_request = new Request(url, request);
+//       return fetch(new_request);
+//     }
+//     return env.ASSETS.fetch(request);
+//   },
+// };
+
+export default {
+  async fetch(request, env) {
+    let url = new URL(request.url);
+    if (url.pathname.startsWith('/')) {
+      url.hostname = 'source.unsplash.com'
+      let new_request = new Request(url, request);
+      const res = await fetch(new_request);
+      if (res.status === 302) {
         return fetch(res.headers.get('location'));
+      }
+      return res;
     }
-    return res
-  }
-  event.respondWith(handleRequest(request));
-});
+    return env.ASSETS.fetch(request);
+  },
+};
